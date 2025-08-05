e9326bbe0844b579b144a9eda80e0bc986454df5
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-08-05 17:53:25 -0300

    feat: refatora componente NewTransaction para usar modal

--- src/components/NewTransaction/NewTransaction.tsx
+++ src/components/NewTransaction/NewTransaction.tsx
@@ -1,4 +1,5 @@
 import { EnterEvent } from '../../utils'
+import Modal from '../Modal'
 
 import { useNewTransaction } from './useNewTransaction'
 
@@ -6,30 +7,50 @@ export function NewTransaction() {
   const { refs, submit, openForm, formOpened, closeForm } = useNewTransaction()
 
   return (
-    <div id='new-transaction' className={`${formOpened ? 'form-opened' : ''}`}>
-      <div className='front'>
-        <button id='open-form' onClick={openForm}>Adicionar</button>
-      </div>
-      <div className='back'>
-        <input
-          id='description'
-          ref={refs.description}
-          type='text'
-          placeholder='Descrição'
-          onKeyDown={EnterEvent(submit)}
-        />
-        <input
-          id='value'
-          ref={refs.value}
-          type='number'
-          placeholder='Valor'
-          onKeyDown={EnterEvent(submit)}
-        />
-        <div id='actions'>
-          <button onClick={closeForm} id='button-cancel'><i className='fa-solid fa-times' /></button>
-          <button onClick={submit} id='button-submit'><i className='fa-solid fa-plus' /></button>
-        </div>
-      </div>
-    </div>
+    <>
+      <button onClick={openForm}><i className='fa-solid fa-plus' /></button>
+      <Modal.Root opened={formOpened} closeModal={closeForm}>
+        <Modal.Header>
+          <div className='d-flex row justify-between items-center'>
+            <Modal.Title value='Adicionar'/>
+            <button className='close-button' onClick={closeForm}><i className='fa-solid fa-times' /></button>
+          </div>
+        </Modal.Header>
+        <Modal.Content>
+          <div className='d-flex column'>
+            <div className='input-container'>
+              <label htmlFor="description">Descrição</label>
+              <input
+                id='description'
+                name='description'
+                ref={refs.description}
+                type='text'
+                onKeyDown={EnterEvent(submit)}
+                className='mb-3'
+                autoComplete='off'
+                autoFocus
+              />
+            </div>
+            <div className='input-container'>
+              <label htmlFor="value">Valor</label>
+              <input
+                id='value'
+                name='value'
+                ref={refs.value}
+                type='number'
+                onKeyDown={EnterEvent(submit)}
+                autoComplete='off'
+              />
+            </div>
+          </div>
+        </Modal.Content>
+        <Modal.Footer>
+          <div className='d-flex row justify-end'>
+            <Modal.Action action={closeForm} cancel>Cancelar</Modal.Action>
+            <Modal.Action action={submit}>Enviar</Modal.Action>
+          </div>
+        </Modal.Footer>
+      </Modal.Root>
+    </>
   )
 }
