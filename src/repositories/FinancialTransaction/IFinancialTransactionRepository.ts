a4fd2a4bbc97ecb528f87e743237a1a6645a55fc
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-08-06 23:59:13 -0300

    feat: altera métodos save, update e cria init

--- src/repositories/FinancialTransaction/IFinancialTransactionRepository.ts
+++ src/repositories/FinancialTransaction/IFinancialTransactionRepository.ts
@@ -1,9 +1,10 @@
 import type { IFinancialTransaction } from '../../models'
 
 export interface IFinancialTransactionRepository {
+  init(): void
   findAll(): Promise<IFinancialTransaction[]>
   findById(id: IFinancialTransaction['id']): Promise<IFinancialTransaction | undefined>
-  save(transaction: IFinancialTransaction): Promise<void>
-  update(transaction: IFinancialTransaction): Promise<void>
+  save(transaction: IFinancialTransaction): Promise<IFinancialTransaction>
+  update(transaction: IFinancialTransaction): Promise<IFinancialTransaction>
   delete(transaction: IFinancialTransaction): Promise<boolean>
 }
